2dcc778d64b8cc58a6f5fc62f77f9858c051625f
Guillaume Wells <wellsguillaume@gmail.com>
2018-12-11 21:57:30 +0100

    graphql #Wip

--- src/Vue/App.vue
+++ src/Vue/App.vue
@@ -2,11 +2,13 @@
     <div id="app">
         <h1 class="title">Monitor Github Traffic</h1>
         <img src="../Assets/GitHub.jpg"/>
+        {{ user }}
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import gql from 'graphql-tag'
 
 export default {
     name: 'App',
@@ -24,8 +26,21 @@ export default {
         axios
             .get('https://api.github.com', {headers : {'Authorization' : 'token ' + localStorage.token}})
             .then(response => this.info = response)
-        */
-  }
+         */
+    },
+    apollo: {
+        user : gql`{
+            user(login: "bulliby")
+            {
+                name
+                repositories(first:10){
+                    nodes {
+                        name
+                    }
+                }
+            }
+        }`
+    }
 }
 </script>
 
--- src/Vue/app.js
+++ src/Vue/app.js
@@ -1,8 +1,50 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from "vue-apollo"
 
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
+
+
+// HTTP connexion to the API
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'https://api.github.com/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  console.log(token);
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+})
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 const app = new Vue({
     el: '#app',
+    // inject apolloProvider here like vue-router or vuex
+    apolloProvider,
     render: h => h(App),
 });
