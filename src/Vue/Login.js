d0bf6d5f70626f56a0eed7b8c3baf6c92d86ed3d
Guillaume Wells <wellsguillaume@gmail.com>
2019-01-19 15:31:15 +0100

    feat(master): WORKS I get the code from Github

--- src/Vue/Login.js
+++ src/Vue/Login.js
@@ -2,13 +2,13 @@ import auth0 from 'auth0-js';
 
 var auth = new auth0.WebAuth({
     domain: "bulliby.eu.auth0.com",
-    clientID: "613wYDuqtCAY2Xe8Q6iR2sy2R51OcKD1"
+    clientID: "613wYDuqtCAY2Xe8Q6iR2sy2R51OcKD1",
 });
 
-export function login() {
+export default function login() {
     auth.authorize({
         responseType: 'code',
         scope: 'public_repo',
-        redirectUri: 'https://gitgraph.wellsguillaume.fr'
+        connection: 'github'
     });
 }
