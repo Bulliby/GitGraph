380cae4063c6ef2ccfc33295cbcc8ab342c4e663
Guillaume Wells <wellsguillaume@gmail.com>
2022-06-24 22:17:10 +0200

    feat(refacto-front): update npm packages

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
 const env = dotenv.config({ path: path.resolve(process.cwd(), 'config/dev.env.js') }).parsed;
 
