9a4372774a58bef41de94cf350fa4e2950dddda7
Guillaume Wells <wellsguillaume@gmail.com>
2022-11-26 16:17:07 +0100

    feat(master): url change in config prod

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 REDIRECT_URL="https://gitgraph.wellsguillaume.fr"
 CLIENT_ID="3c47a9a8faf9b82f5634"
-OAUTH_URL="https://server.wellsguillaume.fr"
+OAUTH_URL="https://server.gitgraph.wellsguillaume.fr"
 //Domain who is set for the cookie
-DOMAIN="wellsguillaume.fr"
+DOMAIN="gitgraph.wellsguillaume.fr"
